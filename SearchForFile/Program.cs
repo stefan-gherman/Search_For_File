5a3f36364b7946b9ece04d930357fd61efe4a2e9
Stefan Gherman <scgheman@gmail.com>
2020-06-29 08:11:37 +0300

    Add project files.

---
+++ SearchForFile/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace SearchForFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetFilePath(@args[0], @args[1]);
+
+            WatchFile(@args[0], @args[1]);
+            Console.ReadKey();
+        }
+
+
+        private static void GetFilePath(string dir, string fileName)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(dir, fileName, SearchOption.AllDirectories);
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"File not found!\n{e.Message}");
+            }
+
+        }
+
+        private static void WatchFile(string dir, string filename)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = dir;
+            watcher.Filter = filename;
+
+            watcher.Changed += Watcher_Changed;
+        }
+
+        private static void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+        }
+    }
+}
